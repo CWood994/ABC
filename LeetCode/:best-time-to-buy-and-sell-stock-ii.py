ee60f7188907293789ba855b630905bda4ea5624
Connor Wood <connwood@cisco.com>
2017-12-14 12:33:50 -0800

    Best Time to Buy and Sell Stock II

---
+++ LeetCode/:best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        if prices is None or len(prices) == 0:
+            return 0
+        
+        profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                profit += prices[i+1]-prices[i]
+        
+        return profit
